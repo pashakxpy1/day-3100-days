d23bf213a7279d8dd358e3d15e2ee4ecb4a10fe6
pashakxpy1 <pasha.kxpay@gmail.com>
2025-01-26 18:44:30 +0000

    fin

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-print("=== Your Song Generator ===")
-print("""You'll be asked a bunch of questions
-then we'll make you up an amazing
-song, totally copyright free 😭""")
-print()
-
-person = input("Name a person famous for something good: ")
-thing = input("Name a thing they did: ")
-place = input("Name a place you like: ")
-rhyme = input("Give me a verb that rhymes with your person's name: ")
+food = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("What is a way to cook something? ")
+burntFood = input("How do you describe burnt food? ")
+householdItem = input("Name something in your house: ")
 
 print()
-print("There was a person called", person, "Who did something cool like", thing, "at the wonderful", place,  "where you'll find me", rhyme)
+print("Tonight's dinner:")
+
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
--- readme.md
+++ readme.md
@@ -18,3 +18,5 @@ group = input("Give me a collective noun for a group of things: ")
 thing = input("Give me the name of a weird or wacky thing: ")
 print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
 ```
+
+правилькний приклад в main.py
